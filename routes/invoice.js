5f78a3b0a0d469b10636a0f448679c4738e5d809
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2023-02-16 20:21:35 +0100

    delete route

--- routes/invoice.js
+++ routes/invoice.js
@@ -13,8 +13,8 @@ router.route("/preview/:id").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/add").post((req, res) => {
-  const status = req.body.status;
+router.route("/add/draft").post((req, res) => {
+  const status = "draft";
   const street = req.body.street;
   const city = req.body.city;
   const postcode = req.body.postcode;
@@ -50,50 +50,49 @@ router.route("/add").post((req, res) => {
 
   newInvoice
     .save()
-    .then(() => res.json("Invoice added!"))
+    .then(() => res.json("Invoice draft added!"))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/add/draft").post((req, res) => {
-  // const status = req.body.status;
-  // const street = req.body.street;
-  // const city = req.body.city;
-  // const postcode = req.body.postcode;
-  // const country = req.body.country;
-  // const client_name = req.body.client_name;
-  // const client_email = req.body.client_email;
-  // const client_street = req.body.client_street;
-  // const client_city = req.body.client_city;
-  // const client_postcode = req.body.client_postcode;
-  // const client_country = req.body.client_country;
-  // const invoice_date = Date.parse(req.body.invoice_date);
-  // const payment_date = Date.parse(req.body.payment_date);
-  // const project_description = req.body.project_description;
-  // const item_list = req.body.item_list;
+router.route("/add/pending").post((req, res) => {
+  const status = "pending";
+  const street = req.body.street;
+  const city = req.body.city;
+  const postcode = req.body.postcode;
+  const country = req.body.country;
+  const client_name = req.body.client_name;
+  const client_email = req.body.client_email;
+  const client_street = req.body.client_street;
+  const client_city = req.body.client_city;
+  const client_postcode = req.body.client_postcode;
+  const client_country = req.body.client_country;
+  const invoice_date = Date.parse(req.body.invoice_date);
+  const payment_date = Date.parse(req.body.payment_date);
+  const project_description = req.body.project_description;
+  const item_list = req.body.item_list;
 
-  // const newInvoice = new Invoice({
-  //   status,
-  //   street,
-  //   city,
-  //   postcode,
-  //   country,
-  //   client_name,
-  //   client_email,
-  //   client_street,
-  //   client_city,
-  //   client_postcode,
-  //   client_country,
-  //   invoice_date,
-  //   payment_date,
-  //   project_description,
-  //   item_list,
-  // });
+  const newInvoice = new Invoice({
+    status,
+    street,
+    city,
+    postcode,
+    country,
+    client_name,
+    client_email,
+    client_street,
+    client_city,
+    client_postcode,
+    client_country,
+    invoice_date,
+    payment_date,
+    project_description,
+    item_list,
+  });
 
-  // newInvoice
-  //   .save()
-  // .then(() => res.json("Draft!"))
-  // .catch((err) => res.status(400).json("Error: " + err));
-  console.log("draft", req);
+  newInvoice
+    .save()
+    .then(() => res.json("Invoice pending!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/edit/:id").get((req, res) => {
@@ -141,7 +140,7 @@ router.route("/status/:id").put((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/:id").delete((req, res) => {
+router.route("/preview/:id").delete((req, res) => {
   Invoice.findByIdAndDelete(req.params.id)
     .then(() => res.json("Invoice deleted"))
     .catch((err) => res.status(400).json("Error: " + err));
